682b7fe6f4b37f4667889086c7e8696b1be73df0
Exequiel Marichal <exemarichal@gmail.com>
2021-06-07 21:51:47 -0300

    subir trabajo

--- app/src/main/java/com/example/tarso/MenuEstudiantesActivity.java
+++ app/src/main/java/com/example/tarso/MenuEstudiantesActivity.java
@@ -1,6 +1,7 @@
 package com.example.tarso;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -9,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.Manifest;
 import android.app.Dialog;
 import android.app.DownloadManager;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -24,6 +26,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -47,10 +50,16 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MenuEstudiantesActivity extends AppCompatActivity {
 
@@ -70,9 +79,16 @@ public class MenuEstudiantesActivity extends AppCompatActivity {
     RecyclerView recycler_trabajos;
     LinearLayoutManager layoutManager;
     FirebaseFirestore db;
+    StorageReference storageReference;
     FirestoreRecyclerAdapter<Documentos, DocumentosViewHolder> adapter;
 
     private FirebaseAuth mAuth;
+    EditText edtNombreCreador, edtNombreTrabajo;
+    Spinner spin_curso_agregar_trab, spin_asig_agregar_trab;
+    Button btnSelect, btnUpload;
+    String cur="";
+    String mat="";
+    Documentos document;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,11 +98,12 @@ public class MenuEstudiantesActivity extends AppCompatActivity {
         txtNombre = findViewById(R.id.nombrePersona);
         btnVerTrabajos = findViewById(R.id.btnTrabajos);
         btnVerMisTrabajos = findViewById(R.id.btnVerMisTrabajos);
-        btnSubirTrabajos = findViewById(R.id.btnSubirTrabajo);
+        btnSubirTrabajos = findViewById(R.id.btnSubirMisTrabajos);
         btnSalir = findViewById(R.id.salir);
 
         mAuth = FirebaseAuth.getInstance();
         db = FirebaseFirestore.getInstance();
+        storageReference = FirebaseStorage.getInstance().getReference("documentos");
 
         if (getIntent()!=null){
             id = getIntent().getStringExtra("IDESTUDIANTE");
@@ -180,7 +197,154 @@ public class MenuEstudiantesActivity extends AppCompatActivity {
 
     private void dialogSubirTrabajo() {
         //agregar el dialog para subir un pdf del menuDocentes
+        final AlertDialog.Builder alerta = new AlertDialog.Builder(MenuEstudiantesActivity.this)
+                .setTitle("Subir trabajo ... ")
+                .setCancelable(false);
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View agregar_trabajo = inflater.inflate(R.layout.agregar_trabajo, null);
+
+        edtNombreTrabajo = agregar_trabajo.findViewById(R.id.edtNombreTrabajo);
+        edtNombreCreador = agregar_trabajo.findViewById(R.id.edtNombreCreador);
+
+        //Spinners
+        spin_curso_agregar_trab = agregar_trabajo.findViewById(R.id.spinCurso);
+        spin_asig_agregar_trab = agregar_trabajo.findViewById(R.id.spinMateria);
+        ArrayAdapter<CharSequence> adapterCursos = ArrayAdapter.createFromResource(this,
+                R.array.Cursos, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapterAsignatura = ArrayAdapter.createFromResource(this,
+                R.array.Asignaturas, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapterCursos.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapterAsignatura.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spin_curso_agregar_trab.setAdapter(adapterCursos);
+        spin_asig_agregar_trab.setAdapter(adapterAsignatura);
+
+        spin_curso_agregar_trab.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                cur = String.valueOf(parent.getItemIdAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        spin_asig_agregar_trab.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mat = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        btnSelect = agregar_trabajo.findViewById(R.id.btnSelect);
+        btnUpload = agregar_trabajo.findViewById(R.id.btnUpload);
+
+        btnSelect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seleccionarPDF();
+            }
+        });
+
+        alerta.setView(agregar_trabajo);
+        alerta.setPositiveButton("Agregar archivo", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (edtNombreCreador.getText().toString().equals("")
+                        || edtNombreTrabajo.getText().toString().equals("")
+                        || mat.equals("") || mat.equals("Seleccione") || cur.equals("") || cur.equals("0")) {
+                    Toast.makeText(MenuEstudiantesActivity.this, "Complete todos los campos", Toast.LENGTH_SHORT).show();
+                } else if (document != null){
+                    String uid = String.valueOf(System.currentTimeMillis());
+                    db.collection("Documentos").document(uid).set(document);
+                }
+            }
+        });
+
+        alerta.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        alerta.show();
     }
+
+    private void seleccionarPDF() {
+        Intent selecWork = new Intent();
+        selecWork.setType("application/pdf");
+        selecWork.setAction(selecWork.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(selecWork, "SELECCIONAR PDF"), 12);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 12 && resultCode == RESULT_OK && data != null && data.getData() != null){
+            btnUpload.setEnabled(true);
+            btnSelect.setText("Archivo selec.");
+
+            btnUpload.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    subirArchivoPDF(data.getData());
+                    Log.d("Nombre uri", data.toString());
+                }
+            });
+
+        }
+    }
+
+    private void subirArchivoPDF(Uri data) {
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Subir archivo");
+        progressDialog.show();
+
+        StorageReference reference = storageReference.child("materia/" + System.currentTimeMillis() + ".pdf");
+
+        reference.putFile(data)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                        while (!uriTask.isComplete());
+                        Uri uri = uriTask.getResult();
+
+                        SimpleDateFormat currentDate = new SimpleDateFormat("dd/MM/yyyy");
+                        Date todayDate = new Date();
+                        String thisDate = currentDate.format(todayDate);
+
+                        document = new Documentos(edtNombreTrabajo.getText().toString(), cur, mat, thisDate, edtNombreCreador.getText().toString(), uri.toString(), id);
+
+
+
+                        Toast.makeText(MenuEstudiantesActivity.this, "Archivo subido correctamente", Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+
+                    }
+                })
+                .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+                        double progress = (100.0 * snapshot.getBytesTransferred())/snapshot.getTotalByteCount();
+                        progressDialog.setMessage("Subiendo archivo... " + (int) progress + "%");
+
+                    }
+                });
+    }
+
 
     private void buscarTrabajosDialog(String c, String a, String id) {
         final AlertDialog.Builder alert = new AlertDialog.Builder(MenuEstudiantesActivity.this)
@@ -349,8 +513,6 @@ public class MenuEstudiantesActivity extends AppCompatActivity {
         downloadmanager.enqueue(request);
     }
 
-
-
     private void cargarUsuario(String id) {
         db.collection("Usuarios")
                 .whereEqualTo("id", id)
@@ -487,7 +649,6 @@ public class MenuEstudiantesActivity extends AppCompatActivity {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     }
-
 
     @Override
     protected void onStop() {
